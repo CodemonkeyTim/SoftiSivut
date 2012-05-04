9bb96aa75a77f6983538f1b229a22dfeebc19c17
Teemu Vainio <tim@silvertreksystems.com>
2012-05-04 10:20:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoftiSivut::Application.config.secret_token = '5e8522cdf3949262fda88118ee82197824fa80f939103f989dd76f7595c83abc52d2ba5f2178fee39a5468e81d57fdaf2e0e128fa888019cc2551b7712d09e37'
