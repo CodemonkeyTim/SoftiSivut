071697bd40daca98c26662c75cbd3320d3742fab
Teemu Vainio <tim@silvertreksystems.com>
2012-05-05 04:55:28 -0700

    alotta stoof

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -1,4 +1,29 @@
 class PublicPagesController < ApplicationController
   def index
+    
+  end
+  
+  def pelit
+    
+  end
+  
+  def saannot
+    if params[:id].nil?
+      @title = "Kaiken maailman saantoja"
+    else
+      case params[:id].to_i
+        when 1 then @title = "Jarjestyssaannot"
+        when 2 then @title = "Pelisaannot"  
+        when 3 then @title = "Muita saantoja"
+      end
+    end
+    
+    
+    
+      
+  end
+  
+  def pelipaikat
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SoftiSivut::Application.routes.draw do
   match "/koti" => "public_pages#index"
   match "/pelit" => "public_pages#pelit"
+  match "/saannot" => "public_pages#saannot"
+  match "/saannot/:id" => "public_pages#saannot"
+  match "/pelipaikat" => "public_pages#pelipaikat"
   
   root :to => 'public_pages#index'
 end
