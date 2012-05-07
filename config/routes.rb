66ec9601a0363c3f03ad96c1ffeeb34cce7459e0
Teemu Vainio <tim@silvertreksystems.com>
2012-05-07 07:05:45 -0700

    Yhteystiedot lisatty

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SoftiSivut::Application.routes.draw do
   match "/saannot/:id" => "public_pages#saannot"
   match "/pelipaikat" => "public_pages#pelipaikat"
   match "/foorumi" => "public_pages#foorumi"
+  match "/yhteystietoja" => "public_pages#yhteystietoja"
   
   root :to => 'public_pages#index'
 end
