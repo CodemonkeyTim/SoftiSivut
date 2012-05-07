976db03ade63c33e38b94ef917ed991dbfa84d8a
Teemu Vainio <tim@silvertreksystems.com>
2012-05-07 04:53:22 -0700

    Updated som stoof:

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ SoftiSivut::Application.routes.draw do
   match "/saannot" => "public_pages#saannot"
   match "/saannot/:id" => "public_pages#saannot"
   match "/pelipaikat" => "public_pages#pelipaikat"
+  match "/foorumi" => "public_pages#foorumi"
   
   root :to => 'public_pages#index'
 end
